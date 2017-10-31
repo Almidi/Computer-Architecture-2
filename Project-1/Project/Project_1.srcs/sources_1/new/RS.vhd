c0a1bee7040a500be90f28603301af714d26080f
almidi <vailakis7@yahoo.gr>
2017-10-31 16:25:19 +0200

    fix RS

--- Project-1/Project/Project_1.srcs/sources_1/new/RS.vhd
+++ Project-1/Project/Project_1.srcs/sources_1/new/RS.vhd
@@ -1,5 +1,5 @@
--- Authors: Tzanis Fotakis
---			Apostolos Vailakis
+-- Authors:   Tzanis Fotakis
+--            Apostolos Vailakis
 
 
 library IEEE;
@@ -8,20 +8,21 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity RS is
     Port ( WrEn : in STD_LOGIC;
-           Op : in STD_LOGIC_VECTOR (1 downto 0);
-           Vj : in STD_LOGIC_VECTOR (31 downto 0);
-           Vk : in STD_LOGIC_VECTOR (31 downto 0);
-           Qj : in STD_LOGIC_VECTOR (4 downto 0);
-           Qk : in STD_LOGIC_VECTOR (4 downto 0);
-           OpOut : out STD_LOGIC_VECTOR (1 downto 0);
-           VjOut : out STD_LOGIC_VECTOR (31 downto 0);
-           VkOut : out STD_LOGIC_VECTOR (31 downto 0);
-           ReadyOut : out STD_LOGIC;
-           CDBQ : in STD_LOGIC_VECTOR (4 downto 0);
-           CDBV : in STD_LOGIC_VECTOR (31 downto 0);
-           BusyOut : out STD_LOGIC;
-           RST : in STD_LOGIC;
-           CLK : in STD_LOGIC);
+           Op : in STD_LOGIC_VECTOR (1 downto 0);       -- Operation input
+           Vj : in STD_LOGIC_VECTOR (31 downto 0);      -- Vj Input
+           Vk : in STD_LOGIC_VECTOR (31 downto 0);      -- Vk Input
+           Qj : in STD_LOGIC_VECTOR (4 downto 0);       -- Qj Input
+           Qk : in STD_LOGIC_VECTOR (4 downto 0);       -- Qk Input
+           Ex : in STD_LOGIC;                           -- RS Executed
+           OpOut : out STD_LOGIC_VECTOR (1 downto 0);   -- Operation Output
+           VjOut : out STD_LOGIC_VECTOR (31 downto 0);  -- Vj Output 
+           VkOut : out STD_LOGIC_VECTOR (31 downto 0);  -- Vk Output
+           ReadyOut : out STD_LOGIC;                    -- Ready Output
+           CDBQ : in STD_LOGIC_VECTOR (4 downto 0);     -- CDB.Q
+           CDBV : in STD_LOGIC_VECTOR (31 downto 0);    -- CDB.V
+           BusyOut : out STD_LOGIC;                     -- Busy
+           RST : in STD_LOGIC;                          -- RESET
+           CLK : in STD_LOGIC);                         -- Clock
 end RS;
 
 architecture Behavioral of RS is
@@ -50,6 +51,14 @@ component Register2 is
            DataOut : out STD_LOGIC_VECTOR (1 downto 0));
 end component;
 
+component Register1 is
+    Port ( DataIn : in STD_LOGIC;
+           WrEn : in STD_LOGIC;
+           Rst : in STD_LOGIC;
+           Clk : in STD_LOGIC;
+           DataOut : out STD_LOGIC;
+end component;
+
 component CompareModule is
     Port ( In0 : in  STD_LOGIC_VECTOR (4 downto 0);
            In1 : in  STD_LOGIC_VECTOR (4 downto 0);
@@ -65,99 +74,103 @@ SIGNAL VkInput : STD_LOGIC_VECTOR (31 downto 0);
 SIGNAL QjInternal : STD_LOGIC_VECTOR (4 downto 0);
 SIGNAL QkInternal : STD_LOGIC_VECTOR (4 downto 0);
 
-SIGNAL Qj0 : STD_LOGIC; --
-SIGNAL Qk0 : STD_LOGIC; --
+SIGNAL Qj0 : STD_LOGIC;
+SIGNAL Qk0 : STD_LOGIC;
 
-SIGNAL VjWrEN : STD_LOGIC; -- TODO: OR these things correctly
-SIGNAL VkWrEN : STD_LOGIC; --
+SIGNAL VjWrEN : STD_LOGIC;
+SIGNAL VkWrEN : STD_LOGIC;
 
-SIGNAL QjWrEN : STD_LOGIC; -- TODO: OR these things correctly
-SIGNAL QkWrEN : STD_LOGIC; --
+SIGNAL QjWrEN : STD_LOGIC;
+SIGNAL QkWrEN : STD_LOGIC;
 
 SIGNAL Comp1Out : STD_LOGIC;
 SIGNAL Comp2Out : STD_LOGIC;
 SIGNAL Comp3Out : STD_LOGIC;
 
-SIGNAL Inv_Op_Input : STD_LOGIC_VECTOR (1 downto 0);
-SIGNAL Inv_Op_Output : STD_LOGIC_VECTOR (1 downto 0);
+SIGNAL BusyRegIn : STD_LOGIC;
+SIGNAL BusyRegWrEn : STD_LOGIC;
 
 
 begin
+-- Busy Register
+BREG : Register1 Port Map (
+         DataIn =>BusyRegIn,
+         WrEn =>BusyRegWrEn,
+         Clk =>CLK,
+         DataOut =>BusyOut,
+         Rst =>RST);
+
 -- V Registers
 
 VjREG : Register32 Port Map (
-		   DataIn =>VjInput,
-           WrEn =>VjWrEN,
-           Clk =>CLK,
-           DataOut =>VjOut,
-           Rst =>RST);
+         DataIn =>VjInput,
+         WrEn =>VjWrEN,
+         Clk =>CLK,
+         DataOut =>VjOut,
+         Rst =>RST);
 
 VkREG : Register32 Port Map ( 
-		   DataIn =>VkInput,
-           WrEn =>VkWrEN,
-           Clk =>CLK,
-           DataOut =>VkOut,
-           Rst =>RST);
+         DataIn =>VkInput,
+         WrEn =>VkWrEN,
+         Clk =>CLK,
+         DataOut =>VkOut,
+         Rst =>RST);
 
 -- Q Registers
 
 QjREG : Register5 Port Map ( 
-		   DataIn =>QjInput,
-           WrEn =>QjWrEN,
-           Clk =>CLK,
-           DataOut =>QjInternal,
-           Rst =>RST);
+         DataIn =>QjInput,
+         WrEn =>QjWrEN,
+         Clk =>CLK,
+         DataOut =>QjInternal,
+         Rst =>RST);
 
 QkREG : Register5 Port Map ( 
-		   DataIn =>QkInput,
-           WrEn =>QkWrEN,
-           Clk =>CLK,
-           DataOut =>QkInternal,
-           Rst =>RST);
+         DataIn =>QkInput,
+         WrEn =>QkWrEN,
+         Clk =>CLK,
+         DataOut =>QkInternal,
+         Rst =>RST);
 -- OP Register
 
 OpREG : Register2 Port Map ( 
-		   DataIn =>Inv_Op_Input,
-           WrEn =>WrEn,
-           Clk =>CLK,
-           DataOut =>Inv_Op_Output,
-           Rst =>RST);
+         DataIn =>Op,
+         WrEn =>WrEn,
+         Clk =>CLK,
+         DataOut =>OpOut,
+         Rst =>RST);
 -- Comparator
 
 -- CDBQ == Qj
 Comp1 : CompareModule Port Map( 
-		   In0 =>CDBQ,
-           In1 =>QjInternal,
-           DOUT =>Comp1Out );
+         In0 =>CDBQ,
+         In1 =>QjInternal,
+         DOUT =>Comp1Out );
 -- CDBQ == Qk
 Comp2 : CompareModule Port Map( 
-		   In0 =>CDBQ,
-           In1 =>QkInternal,
-           DOUT =>Comp2Out );
+         In0 =>CDBQ,
+         In1 =>QkInternal,
+         DOUT =>Comp2Out );
 -- CDBQ == 00000
 Comp3 : CompareModule Port Map( 
-		   In0 =>CDBQ,
-           In1 =>"00000",
-           DOUT =>Comp3Out );
+         In0 =>CDBQ,
+         In1 =>"00000",
+         DOUT =>Comp3Out );
 
-
--- Input-Output Invertion (For Correct Reset)
-Inv_Op_Input <= NOT Op ;
-OpOut <= NOT Inv_Op_Output ;
-
--- Busy Signal
-BusyOut <= Inv_Op_Output(0) NOR Inv_Op_Output(1) ;
+-- Busy Signal Register
+BusyRegWrEn <= WrEn OR Ex ;
+BusyRegIn <= WrEn ;
 
 -- Ready Signal 
 with QjInternal select
 	Qj0 <=      '0' when std_logic_vector(to_unsigned(0,5)),
-				'1' when others ;
+				      '1' when others ;
 
 with QkInternal select
 	Qk0 <=      '0' when std_logic_vector(to_unsigned(0,5)),
-				'1' when others ;
+				      '1' when others ;
 
-ReadyOut <= Qj0 NOR Qk0 ;
+ReadyOut <= (Qj0 NOR Qk0) AND Busy;
 
 -- Mask Q Register Input when CDBV Arrived
 QjInput(0) <= Qj(0) AND (NOT Comp1Out);
@@ -173,14 +186,13 @@ QkInput(3) <= Qk(3) AND (NOT Comp2Out);
 QkInput(4) <= Qk(4) AND (NOT Comp2Out);
 
 -- Multiplex V Inputs
-
 with Comp1Out select
 	VjInput <=      Vj when '0',
-					CDBV when others ;
+					        CDBV when others ;
 
 with Comp2Out select
 	VkInput <=      Vk when '0',
-					CDBV when others ;
+					        CDBV when others ;
 
 -- Registers WrEn ORing
 
